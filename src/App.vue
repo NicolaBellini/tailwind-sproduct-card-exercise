110c1da507f96ba0c8ba221e0daf33d5db943b9c
NicolaBellini <Bellininicola4366@gmail.com>
2024-07-18 10:31:16 +0200

    scafolding

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import Main from "./components/main.vue";
+export default {
+  components: {
+    Main,
+  },
+};
+</script>
+
+<template class="bg-slate-50 dark:bg-black dark:text-white">
+  <Main />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/main.vue
@@ -0,0 +1,13 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div
+    class="grid justify-items-center items-center bg-customColor text-white sticky top-0 z-10 min-h-screen"
+  >
+    ciaos
+  </div>
+</template>
+
+<style></style>
