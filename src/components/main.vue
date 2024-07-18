f57aa06201138ad8818460cc9a4a87b8aaa93fd6
NicolaBellini <Bellininicola4366@gmail.com>
2024-07-18 11:18:31 +0200

    [add] img and  text befor prices

--- src/components/main.vue
+++ src/components/main.vue
@@ -7,10 +7,28 @@ export default {};
     class="grid justify-items-center items-center bg-customColor text-white sticky top-0 z-10 min-h-screen"
   >
     <div class="flex bg-red-400 w-600 h-450 rounded-xl overflow-hidden">
-      <div class="w-1/2 bg-blue-400 h-full"></div>
-      <div class="w-1/2 bg-green-400 h-full">
-        <div class="font-sans">Questo testo usa il font Montserrat.</div>
-        <div class="font-fraunces">Questo testo usa il font Fraunces.</div>
+      <div class="w-1/2 bg-blue-400 h-full">
+        <img src="../../public/images/image-product-desktop.jpg" alt="" />
+      </div>
+      <div class="w-1/2 bg-white text-black h-full pl-8 pr-8 pt-8">
+        <div
+          class="font-sans tracking-[.60em] text-xs mb-6 text-slate-400 font-semibold"
+        >
+          PERFUME
+        </div>
+        <div class="font-fraunces mb-5 text-4xl font-semibold">
+          Gabrielle Essence Eau de Parfum
+        </div>
+        <div
+          class="font-sans text-[14px] text-slate-400 leading-[1.3rem] font-semibold"
+        >
+          A floreal, solar and voluptuos interpretation composed by Olivier
+          Polge, Perfumer-Creator for the House of CHANEL
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   </div>
